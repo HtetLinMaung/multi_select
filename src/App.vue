aa7c0f9068d7854f1c4037b610cea6ac89262fc9
Jst <jsthtet96@gmail.com>
2019-07-18 21:10:27 +0630

    adding select all functionality

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="ml-5">
+    <MultiSelect />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MultiSelect from "./components/MultiSelect";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    MultiSelect
   }
 };
 </script>
